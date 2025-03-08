1fef90c9d0386194a4c3410703eef5d8cd08a688
zyruscarino5 <zyruspogi213@gmail.com>
2025-03-07 16:41:45 -1100

    Create Carino.java

---
+++ Carino.java
@@ -0,0 +1,8 @@
+public class Carino {
+     public static void display() {
+         int age = 20; // Replace with user input if necessary
+         System.out.println("Hello! This is Adriano contributing to the project.");
+         System.out.println("My current age is: " + age);
+         System.out.println("Next year, I will be: " + (age + 1));
+     }
+ }
